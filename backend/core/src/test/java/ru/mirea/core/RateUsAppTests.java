e9ce333048ab165443aacc698d09087e9115565d
Foksen <izholobov2004@gmail.com>
2025-05-19 01:03:36 +0300

    Add spring container

--- backend/core/src/test/java/ru/mirea/core/RateUsAppTests.java
+++ backend/core/src/test/java/ru/mirea/core/RateUsAppTests.java
@@ -3,7 +3,7 @@ package ru.mirea.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class RateUsAppTests {
 
 	@Test
